3e17672b174a2453f961e3b77d1c23bbd7c5a1b4
elmerahykhadija <elmerahykhadija700@gmail.com>
2026-09-22 16:02:37 +0000

    acceptane rate (pyspak)

--- Pyspark/medium/Acceptance Rate By Date.py
+++ Pyspark/medium/Acceptance Rate By Date.py
@@ -13,3 +13,24 @@ Output the request date and the acceptance rate, sorted by date in ascending ord
 Table
 fb_friend_requests
 """
+# Import your libraries
+import pyspark
+from pyspark.sql.functions import when,max,avg
+df=fb_friend_requests.select(
+    "user_id_sender","user_id_receiver",
+    when(fb_friend_requests["action"]=="sent",fb_friend_requests["date"]).alias("date_sending"),
+    when(fb_friend_requests["action"]=="accepted",fb_friend_requests["date"]).alias("date_accepting")
+    )
+df=df.groupby(["user_id_sender","user_id_receiver"]).agg(
+    max(df["date_sending"]).alias("date_sending"),
+    max(df["date_accepting"]).alias("date_accepting")
+    )
+df=df.withColumn(
+        "n",
+        when(df["date_accepting"].isNotNull() ,1).otherwise(0)
+        )
+df=df.groupby("date_sending").agg(
+    avg("n").alias("average")
+    )
+# To validate your solution, convert your final PySpark df to a pandas df
+df.toPandas()
