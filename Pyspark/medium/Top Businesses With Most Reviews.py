fded711490de0df6f33843122458c99c240d4e98
elmerahykhadija <elmerahykhadija700@gmail.com>
2026-09-24 15:02:49 +0000

    top b with most reviews (pyspark)

---
+++ Pyspark/medium/Top Businesses With Most Reviews.py
@@ -0,0 +1,19 @@
+"""
+Find the top 5 businesses with most reviews. Assume that each row has a unique business_id such that the total reviews for each business is listed on each row. Output the business name along with the total number of reviews and order your results by the total reviews in descending order.
+
+
+If there are ties in review counts, businesses with the same number of reviews receive the same rank, and subsequent ranks are skipped accordingly (e.g., if two businesses tie for rank 4, the next business receives rank 6, skipping rank 5).
+
+Table
+yelp_business
+"""
+import pyspark
+from pyspark.sql.functions import sum
+df=yelp_business.groupby("name").agg(
+    sum("review_count").alias("review_count")
+    )
+df=df.orderBy(df["review_count"].desc()).limit(5)
+
+
+# To validate your solution, convert your final PySpark df to a pandas df
+df.toPandas()
