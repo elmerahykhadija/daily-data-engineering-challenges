b0efb502e8a52d9edaf5c2b74be4fa1f4a581b2e
elmerahykhadija <elmerahykhadija700@gmail.com>
2026-09-25 20:47:52 +0000

    Highest target under manger (pyspark)

--- Pyspark/medium/Highest Target Under Manager.py
+++ Pyspark/medium/Highest Target Under Manager.py
@@ -4,3 +4,17 @@ Identify the employee(s) working under manager manager_id=13 who have achieved t
 Table
 salesforce_employees
 """
+import pyspark
+from pyspark.sql.window import Window
+from pyspark.sql.functions import rank
+# Start writing code
+
+df=salesforce_employees.filter(salesforce_employees["manager_id"]==13)
+window=Window.orderBy(df["target"].desc())
+df=df.withColumn(
+    "n",
+    rank().over(window)
+    )
+df=df.select(["first_name","target"]).filter(df["n"]==1)
+# To validate your solution, convert your final PySpark df to a pandas df
+df.toPandas()
