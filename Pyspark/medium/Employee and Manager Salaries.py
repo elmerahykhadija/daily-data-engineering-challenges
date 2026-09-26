33b112243b75ea4582f38ee8a0454d713dc364f9
elmerahykhadija <elmerahykhadija700@gmail.com>
2026-09-26 19:17:15 +0000

    emploee and manager salaries(pyspark)

--- Pyspark/medium/Employee and Manager Salaries.py
+++ Pyspark/medium/Employee and Manager Salaries.py
@@ -4,3 +4,21 @@ Find employees who are earning more than their managers. Output the employee's f
 Table
 employee
 """
+# Import your libraries
+import pyspark
+from pyspark.sql.functions import col
+
+e=employee.alias("e")
+m=employee.alias("m")
+
+managers= e.join(
+    m,col("e.id")==col("m.manager_id"),"inner").select(["m.manager_id","e.salary"]
+    ).distinct().withColumnRenamed("salary","manager_salary")
+
+
+man=managers.alias("man")
+output=e.join(man,col("e.manager_id")==col("man.manager_id"),"inner")
+
+output=output.filter(col("salary")>col("manager_salary")).select(["first_name","salary"]).distinct()
+# To validate your solution, convert your final PySpark df to a pandas df
+output.toPandas()
