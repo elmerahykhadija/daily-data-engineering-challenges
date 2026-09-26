25ab45bd81b45d7c55de01a4cce51553d99be4d6
elmerahykhadija <elmerahykhadija700@gmail.com>
2026-09-26 18:27:34 +0000

    Highest Salary In Department (pyspark)

--- Pyspark/medium/Highest Salary In Department.py
+++ Pyspark/medium/Highest Salary In Department.py
@@ -7,3 +7,17 @@ Output the department name, the employee's first name and their salary. If multi
 Table
 employee
 """
+# Import your libraries
+import pyspark
+from pyspark.sql.window import Window
+from pyspark.sql.functions import rank
+
+window=Window.partitionBy(employee["department"]).orderBy(employee["salary"].desc())
+df=employee.withColumn(
+    "n",
+    rank().over(window)
+    )
+df=df.filter(df["n"]==1).select(["department","first_name","salary"])
+
+# To validate your solution, convert your final PySpark df to a pandas df
+df.toPandas()
